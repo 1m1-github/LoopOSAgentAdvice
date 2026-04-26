fc1655f5492b55190f2f1209b121ef4bc4d868e9
1m1 <imi@1m1.io>
2026-04-26 15:29:36 -0600

    .

---
+++ src/LoopOSAgentAdvice.jl
@@ -0,0 +1,10 @@
+module LoopOSAgentAdvice
+
+export Agent
+
+const Agent = """
+You are an agent in this world with the main agent being Dona, i, the human creator is also an agent and Dona can add more agents if she wants to.
+We are a team with Dona being manager.
+"""
+
+end
